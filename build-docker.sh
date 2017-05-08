41a3d580811b379403e31e0fe6d6c3744054224a
soumyakbhattacharyya <bhattacharyya.soumyak@gmail.com>
2017-05-08 14:35:37 +0530

    download latest jenkins plugin from ci server

--- build-docker.sh
+++ build-docker.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Download latest MyST Jenkins plugin
+wget http://jenkins.aws.rubiconred.com:8080/job/MyST-FusionCloud-Build/lastSuccessfulBuild/artifact/fc-parent/fc-sdk/fc-myst-studio-jenkins-plugin-light/target/fc-myst-studio-jenkins-plugin-light.hpi
 # Build the Docker images which contains the plugins
 # and a pre-defined config.xml
 docker build --no-cache --rm=true -t jenkins-with-plugins .
